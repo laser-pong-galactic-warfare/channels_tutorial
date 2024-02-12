5082b5ac500daf1d32aa8e8be2670833954ec28f
Lucas Fads <lucasfads@gmail.com>
2024-02-12 14:04:47 -0300

    feat: routing websocket protocol

--- channels_tutorial/asgi.py
+++ channels_tutorial/asgi.py
@@ -9,14 +9,23 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channels_tutorial.settings')
 
 django_asgi_app = get_asgi_application()
 
+import chat.routing
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+    ),
 })
 
