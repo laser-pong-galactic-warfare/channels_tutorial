8d7f62a7ca4594e6724968ea50c9a8c48d8130e9
Lucas Fads <lucasfads@gmail.com>
2024-02-12 14:09:25 -0300

    feat: add channel layer

--- channels_tutorial/settings.py
+++ channels_tutorial/settings.py
@@ -73,6 +73,15 @@ WSGI_APPLICATION = 'channels_tutorial.wsgi.application'
 
 ASGI_APPLICATION = "channels_tutorial.asgi.application"
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
